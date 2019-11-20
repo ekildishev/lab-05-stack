a84991fe11a261b143743683fdf350cd67483c08
ekildishev <ekildisev@gmail.com>
2019-11-20 15:22:05 +0300

    Attempt to fix travis ci cfg

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-set -e
-export GTEST_COLOR=1
-export CTEST_OUTPUT_ON_FAILURE=true
+set -ex
 
-CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
-CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
-CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='`pwd`/tools/polly/gcc-7-cxx17-pic.cmake'"
-CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
-
-cmake -H. -B_builds $CMAKE_OPTS -DBUILD_COVERAGE=ON 
+cmake -H. -B_builds  -DBUILD_COVERAGE=ON
 cmake --build _builds
 cmake --build _builds --target test
 cmake --build _builds --target gcov
